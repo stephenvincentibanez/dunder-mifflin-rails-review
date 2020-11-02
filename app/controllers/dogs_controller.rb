34804bfe634377976eda1c6d1c5105fa6d2efb0c
Stephen Ibanez <sibanez93@Stephens-MacBook-Pro-3.local>
2020-11-02 14:40:38 -0800

    done

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,13 +1,19 @@
 class DogsController < ApplicationController
 
     def index
-        @dogs = Dog.all
+        if params[:sort]
+            @dogs = Dog.all.sort_by do |dog|
+                dog.employees.size
+            end
+        else
+            @dogs = Dog.all
+        end
     end
 
-    def sort
-        Dog.sort_by_employee_count
-        render :index
-    end
+    # def sort
+    #     Dog.sort_by_employee_count
+    #     render :index
+    # end
 
     def show
         @dog = Dog.find(params[:id])
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,10 +2,10 @@ class Dog < ApplicationRecord
 
     has_many :employees
 
-    def self.sort_by_employee_count
-        @dogs = Dog.all.sort_by do |dog|
-            dog.employees.size
-        end
-    end
+    # def self.sort_by_employee_count
+    #     Dog.all.sort_by do |dog|
+    #         dog.employees.size
+    #     end
+    # end
 
 end
